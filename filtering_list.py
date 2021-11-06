5af3ab4e86d3e0c435bfe091977dcd115c7106f7
eleenajose <93275670+eleenajose@users.noreply.github.com>
2021-11-06 12:06:10 +0530

    filtering out negative numbers

---
+++ filtering_list.py
@@ -0,0 +1,7 @@
+list1=[12,-7,5,64,-14]
+filter(lambda x:x>0,list1)
+print(list(filter(lambda x:x>0, list1)))
+    
+list2[12,14,-95,3]
+filter(lambda x:x>0,list2)
+print(list(filter(lambda x:x>0, list2)))
